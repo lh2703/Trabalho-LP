293ff9de26d877c4ecbf0e1003642e363148f31f
Luis Henrique <carvalholuis839@gmail.com>
2025-06-06 11:29:11 -0300

    refactor: correção em fib_iterativo

--- src/fibonacci/fib_iterativo.py
+++ src/fibonacci/fib_iterativo.py
@@ -1,13 +1,15 @@
 def fib_iterativo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+
     a = 0
     b = 1
-    cont = 2
+    count = 2
 
-    while cont < n:
-        temp = a + b
-        a = b
-        b = temp
-        cont +=1
+    while count <= n:
+        a, b = b, a + b
+        count += 1
 
     return b
-
